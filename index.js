46a5641d712ab1b165861dbbf20bcf4093389571
Adam Nyitrai <adam.nyitrai87@gmail.com>
2021-11-24 19:39:56 +0000

    feat: finalizing whatabyte

--- index.js
+++ index.js
@@ -16,12 +16,20 @@ const port = process.env.PORT || "8000";
  *  App Configuration
  */
 
+ app.set("views", path.join(__dirname, "views"));
+ app.set("view engine", "pug");
+ app.use(express.static(path.join(__dirname, "public")));
+
 /**
  * Routes Definitions
  */
 
 app.get("/", (req, res) => {
-    res.status(200).send("WHATABYTE: Food For Devs");
+    res.render("index", { title: "Home" });
+});
+
+app.get("/user", (req, res) => {
+    res.render("user", { title: "Profile", userProfile: { nickname: "Auth0" } });
 });
 
 /**
