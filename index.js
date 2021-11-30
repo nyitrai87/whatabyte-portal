cd55a28061ca4d2469acfdaa6c276621d86ad3ad
Adam Nyitrai <adam.nyitrai87@gmail.com>
2021-11-30 20:23:51 +0000

    feat: started to implement auth0 authentication

--- index.js
+++ index.js
@@ -5,6 +5,12 @@
 const express = require("express");
 const path = require("path");
 
+const expressSession = require("express-session");
+const passport = require("passport");
+const Auth0Strategy = require("passport-auth0");
+
+require("dotenv").config();
+
 /**
  * App Variables
  */
@@ -13,12 +19,58 @@ const app = express();
 const port = process.env.PORT || "8000";
 
 /**
+ * Session Configuration
+ */
+
+const session = {
+    secret: process.env.SESSION_SECRET,
+    cookie: {},
+    resave: false,
+    saveUninitialized: false
+};
+  
+if (app.get("env") === "production") {
+    // Serve secure cookies, requires HTTPS
+    session.cookie.secure = true;
+}
+
+/**
+ * Passport Configuration
+ */
+
+const strategy = new Auth0Strategy(
+    {
+        domain: process.env.AUTH0_DOMAIN,
+        clientID: process.env.AUTH0_CLIENT_ID,
+        clientSecret: process.env.AUTH0_CLIENT_SECRET,
+        callbackURL: process.env.AUTH0_CALLBACK_URL
+    },
+    function(accessToken, refreshToken, extraParams, profile, done) {
+        return done(null, profile);
+    }
+);
+
+/**
  *  App Configuration
  */
 
- app.set("views", path.join(__dirname, "views"));
- app.set("view engine", "pug");
- app.use(express.static(path.join(__dirname, "public")));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(expressSession(session));
+
+passport.use(strategy);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser((user, done) => {
+    done(null, user);
+});
+  
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
 
 /**
  * Routes Definitions
